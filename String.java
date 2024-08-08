2c966b3f307d1ebf464ec9e043353ba89b4f69d5
Mukesh Sardiwal <142798358+mukeshsardiwal@users.noreply.github.com>
2024-08-08 11:57:19 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main(String[] args)
+  {
+  String name = "MUZZ";
+  System.out.println(name);
+  System.out.println("Muzz");
+
+  String anothername = "Muzz";
+  String thirdname = new String ("Muzz");
+
+  System.out.println(name == anothername);
+  System.out.println(name == thirdname);
+  System.out.println(name.equals(anothername));
+  String fullname == name + "Sardiwal";
+  System.out.println(fullname);
+    
+  }
+}
